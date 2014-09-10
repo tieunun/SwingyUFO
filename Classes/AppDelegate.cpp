9b7802e693428bd35d32c05c854db2660ec5681c
Casey Caldwell <casecald@gmail.com>
2014-09-09 22:22:44 -0600

    Added scale factor for positions, sizes, etc

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -20,6 +20,32 @@ bool AppDelegate::applicationDidFinishLaunching() {
         glview = GLView::create("Game");
         director->setOpenGLView(glview);
     }
+    
+    Size screenSize = glview->getFrameSize();
+    Size designSize = Size(480, 800);
+    Size adjustedSize = director->getWinSize();
+
+    CCLOG("actual screen width: %f, actual screen height: %f", screenSize.width, screenSize.height);
+    CCLOG("design screen width: %f, design screen height: %f", designSize.width, designSize.height);
+    CCLOG("adjusted screen width: %f, adjusted screen height: %f", adjustedSize.width, adjustedSize.height);
+    
+    glview->setDesignResolutionSize(designSize.width, designSize.height, ResolutionPolicy::FIXED_HEIGHT);
+    
+    // Determine what scale factor will be used for positions, sizes, etc.
+    float scale = 1.0f;
+    
+    if (screenSize.height > 1024) {
+        scale = 2.0f;
+        
+    } else if (screenSize.height > 480) {
+        scale = 1.0f;
+        
+    } else {
+        scale = 0.5f;
+    }
+    
+    // Set the scale factor and the search paths
+    director->setContentScaleFactor(scale);
 
     // Turn on display FPS
     director->setDisplayStats(true);
